efb88969379cd5b959631307cd48d86d03215d47
bad-coco <146406262+bad-coco@users.noreply.github.com>
2026-04-20 21:29:14 +0530

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int, vector<int>>> maxHeap;
+        vector<vector<int>> result;
+
+        for (auto point : points) {
+            int x = point[0];
+            int y = point[1];
+            int distance = sqrt((x * x) + (y * y));
+
+            maxHeap.push({distance, point});
+            if (maxHeap.size() > k) {
+                maxHeap.pop();
+            }
+        }
+
+        while (!maxHeap.empty()) {
+            vector<int> point = maxHeap.top().second;
+            maxHeap.pop();
+            result.push_back(point);
+        }
+        return result;
+    }
+};
